56f88128a1eee5c79276abdfd42150482ec90897
zugz (tox) <mbays+tox@sdf.org>
2020-12-29 00:00:00 +0000

    expose create_dht_packet in DHT.h

--- toxcore/DHT.h
+++ toxcore/DHT.h
@@ -172,15 +172,26 @@ non_null() const Client_data *dht_friend_client(const DHT_Friend *dht_friend, si
  */
 int packed_node_size(Family ip_family);
 
-/** @brief Packs an IP_Port structure into data of max size length.
+/** @brief Pack an IP_Port structure into data of max size length.
  *
  * Packed_length is the offset of data currently packed.
  *
- * @return size of packed IP_Port data on success
+ * @return size of packed IP_Port data on success.
  * @retval -1 on failure.
  */
 non_null()
 int pack_ip_port(const Logger *logger, uint8_t *data, uint16_t length, const IP_Port *ip_port);
+
+/** @brief Encrypt plain and write resulting DHT packet into packet with max size length.
+ *
+ * @return size of packet on success.
+ * @retval -1 on failure.
+ */
+non_null()
+int dht_create_packet(const uint8_t public_key[CRYPTO_PUBLIC_KEY_SIZE],
+                      const uint8_t *shared_key, const uint8_t type,
+                      const uint8_t *plain, size_t plain_length,
+                      uint8_t *packet, size_t length);
 
 /** @brief Unpack IP_Port structure from data of max size length into ip_port.
  *
