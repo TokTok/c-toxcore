1f7d8bb7a59fc8e98a8f81f4e0df4f42b7fbd18a
zugz (tox) <mbays+tox@sdf.org>
2020-12-31 00:00:00 +0000

    feat: add forwarding and announce request handling
    This is the "server-side" part of the new friend finding system,
    allowing DHT nodes to store small amounts of data and permit searching
    for it. A forwarding (proxying) mechanism allows this to be used by TCP
    clients, and deals with non-transitivity in the network.

--- toxcore/crypto_core_test.cc
+++ toxcore/crypto_core_test.cc
@@ -10,6 +10,8 @@
 
 namespace {
 
+using SymmetricKey = std::array<uint8_t, CRYPTO_SYMMETRIC_KEY_SIZE>;
+using Hmac = std::array<uint8_t, CRYPTO_HMAC_SIZE>;
 using ExtPublicKey = std::array<uint8_t, EXT_PUBLIC_KEY_SIZE>;
 using ExtSecretKey = std::array<uint8_t, EXT_SECRET_KEY_SIZE>;
 using Signature = std::array<uint8_t, CRYPTO_SIGNATURE_SIZE>;
@@ -62,12 +64,34 @@ TEST(CryptoCore, Signatures)
 
     // Try a few different sizes, including empty 0 length message.
     for (uint8_t i = 0; i < 100; ++i) {
-        message.push_back(random_u08(rng));
         Signature signature;
         EXPECT_TRUE(crypto_signature_create(
             signature.data(), message.data(), message.size(), get_sig_sk(sk.data())));
         EXPECT_TRUE(crypto_signature_verify(
             signature.data(), message.data(), message.size(), get_sig_pk(pk.data())));
+
+        message.push_back(random_u08(rng));
+    }
+}
+
+TEST(CryptoCore, Hmac)
+{
+    const Random *rng = system_random();
+    ASSERT_NE(rng, nullptr);
+
+    // TODO(zugz): Is symmetric key the right kind of key for the hmac functions?
+    SymmetricKey sk;
+    new_symmetric_key(rng, sk.data());
+
+    std::vector<uint8_t> message;
+
+    // Try a few different sizes, including empty 0 length message.
+    for (uint8_t i = 0; i < 100; ++i) {
+        Hmac auth;
+        crypto_hmac(auth.data(), sk.data(), message.data(), message.size());
+        EXPECT_TRUE(crypto_hmac_verify(auth.data(), sk.data(), message.data(), message.size()));
+
+        message.push_back(random_u08(rng));
     }
 }
 
