0b3b10c1d844e406f3ed9c64506d4aad577acb78
jfreegman <jfreegman@gmail.com>
2021-11-26 12:11:08 -0500

    Add custom and private messages to group message autotest

--- auto_tests/group_message_test.c
+++ auto_tests/group_message_test.c
@@ -16,10 +16,12 @@
 typedef struct State {
     uint32_t index;
     uint64_t clock;
+    uint32_t peer_id;
     bool peer_joined;
     bool message_sent;
     bool message_received;
-
+    bool private_message_received;
+    size_t custom_packets_received;
     bool lossless_check;
     int last_msg_recv;
 } State;
@@ -29,17 +31,31 @@ typedef struct State {
 
 #define NUM_GROUP_TOXES 2
 #define MAX_NUM_MESSAGES 1000
+
 #define TEST_MESSAGE "Where is it I've read that someone condemned to death says or thinks, an hour before his death, that if he had to live on some high rock, on such a narrow ledge that he'd only room to stand, and the ocean, everlasting darkness, everlasting solitude, everlasting tempest around him, if he had to remain standing on a square yard of space all his life, a thousand years, eternity, it were better to live so than to die at once. Only to live, to live and live! Life, whatever it may be!"
+#define TEST_MESSAGE_LEN (sizeof(TEST_MESSAGE) - 1)
+
 #define TEST_GROUP_NAME "Utah Data Center"
+#define TEST_GROUP_NAME_LEN (sizeof(TEST_GROUP_NAME) - 1)
+
+#define TEST_PRIVATE_MESSAGE "Don't spill yer beans"
+#define TEST_PRIVATE_MESSAGE_LEN (sizeof(TEST_PRIVATE_MESSAGE) - 1)
+
+#define TEST_CUSTOM_PACKET "Why'd ya spill yer beans?"
+#define TEST_CUSTOM_PACKET_LEN (sizeof(TEST_CUSTOM_PACKET) - 1)
+
 #define PEER0_NICK "Victor"
+#define PEER0_NICK_LEN (sizeof(PEER0_NICK) - 1)
+
 #define PEER1_NICK "George"
+#define PEER1_NICK_LEN (sizeof(PEER1_NICK) - 1)
 
 static void group_invite_handler(Tox *tox, uint32_t friend_number, const uint8_t *invite_data, size_t length,
                                  const uint8_t *group_name, size_t group_name_length, void *user_data)
 {
     printf("invite arrived; accepting\n");
     TOX_ERR_GROUP_INVITE_ACCEPT err_accept;
-    tox_group_invite_accept(tox, friend_number, invite_data, length, (const uint8_t *)PEER0_NICK, strlen(PEER0_NICK),
+    tox_group_invite_accept(tox, friend_number, invite_data, length, (const uint8_t *)PEER0_NICK, PEER0_NICK_LEN,
                             nullptr, 0, &err_accept);
     ck_assert(err_accept == TOX_ERR_GROUP_INVITE_ACCEPT_OK);
 }
@@ -54,15 +70,54 @@ static void group_peer_join_handler(Tox *tox, uint32_t groupnumber, uint32_t pee
     State *state = (State *)user_data;
     printf("peer %u joined, sending message\n", peer_id);
     state->peer_joined = true;
+    state->peer_id = peer_id;
+}
+
+static void group_custom_packet_handler(Tox *tox, uint32_t groupnumber, uint32_t peer_id, const uint8_t *data,
+                                        size_t length, void *user_data)
+{
+    ck_assert_msg(length == TEST_CUSTOM_PACKET_LEN, "Failed to receive custom packet. Invalid length: %zu\n", length);
+
+    char message_buf[TOX_MAX_CUSTOM_PACKET_SIZE + 1];
+    memcpy(message_buf, data, length);
+    message_buf[length] = 0;
+
+    TOX_ERR_GROUP_PEER_QUERY q_err;
+    size_t peer_name_len = tox_group_peer_get_name_size(tox, groupnumber, peer_id, &q_err);
+
+    ck_assert(q_err == TOX_ERR_GROUP_PEER_QUERY_OK);
+    ck_assert(peer_name_len <= TOX_MAX_NAME_LENGTH);
+
+    char peer_name[TOX_MAX_NAME_LENGTH + 1];
+    tox_group_peer_get_name(tox, groupnumber, peer_id, (uint8_t *) peer_name, &q_err);
+    peer_name[peer_name_len] = 0;
+
+    ck_assert(q_err == TOX_ERR_GROUP_PEER_QUERY_OK);
+    ck_assert(memcmp(peer_name, PEER1_NICK, peer_name_len) == 0);
+
+    TOX_ERR_GROUP_SELF_QUERY s_err;
+    size_t self_name_len = tox_group_self_get_name_size(tox, groupnumber, &s_err);
+    ck_assert(s_err == TOX_ERR_GROUP_SELF_QUERY_OK);
+    ck_assert(self_name_len <= TOX_MAX_NAME_LENGTH);
+
+    char self_name[TOX_MAX_NAME_LENGTH + 1];
+    tox_group_self_get_name(tox, groupnumber, (uint8_t *) self_name, &s_err);
+    self_name[self_name_len] = 0;
+
+    ck_assert(s_err == TOX_ERR_GROUP_SELF_QUERY_OK);
+    ck_assert(memcmp(self_name, PEER0_NICK, self_name_len) == 0);
+
+    printf("%s sent custom packet to %s: %s\n", peer_name, self_name, message_buf);
+    ck_assert(memcmp(message_buf, TEST_CUSTOM_PACKET, length) == 0);
+
+    State *state = (State *)user_data;
+    ++state->custom_packets_received;
 }
 
 static void group_message_handler(Tox *tox, uint32_t groupnumber, uint32_t peer_id, TOX_MESSAGE_TYPE type,
                                   const uint8_t *message, size_t length, void *user_data)
 {
-    if (length > TOX_MAX_MESSAGE_LENGTH) {
-        printf("Failed to receive message. Invalid length: %zu\n", length);
-        return;
-    }
+    ck_assert_msg(length == TEST_MESSAGE_LEN, "Failed to receive message. Invalid length: %zu\n", length);
 
     char message_buf[TOX_MAX_MESSAGE_LENGTH + 1];
     memcpy(message_buf, message, length);
@@ -100,10 +155,53 @@ static void group_message_handler(Tox *tox, uint32_t groupnumber, uint32_t peer_
     state->message_received = true;
 }
 
+static void group_private_message_handler(Tox *tox, uint32_t groupnumber, uint32_t peer_id, TOX_MESSAGE_TYPE type,
+        const uint8_t *message, size_t length, void *user_data)
+{
+    ck_assert_msg(length == TEST_PRIVATE_MESSAGE_LEN, "Failed to receive message. Invalid length: %zu\n", length);
+
+    char message_buf[TOX_MAX_MESSAGE_LENGTH + 1];
+    memcpy(message_buf, message, length);
+    message_buf[length] = 0;
+
+    TOX_ERR_GROUP_PEER_QUERY q_err;
+    size_t peer_name_len = tox_group_peer_get_name_size(tox, groupnumber, peer_id, &q_err);
+
+    ck_assert(q_err == TOX_ERR_GROUP_PEER_QUERY_OK);
+    ck_assert(peer_name_len <= TOX_MAX_NAME_LENGTH);
+
+    char peer_name[TOX_MAX_NAME_LENGTH + 1];
+    tox_group_peer_get_name(tox, groupnumber, peer_id, (uint8_t *) peer_name, &q_err);
+    peer_name[peer_name_len] = 0;
+
+    ck_assert(q_err == TOX_ERR_GROUP_PEER_QUERY_OK);
+    ck_assert(memcmp(peer_name, PEER1_NICK, peer_name_len) == 0);
+
+    TOX_ERR_GROUP_SELF_QUERY s_err;
+    size_t self_name_len = tox_group_self_get_name_size(tox, groupnumber, &s_err);
+    ck_assert(s_err == TOX_ERR_GROUP_SELF_QUERY_OK);
+    ck_assert(self_name_len <= TOX_MAX_NAME_LENGTH);
+
+    char self_name[TOX_MAX_NAME_LENGTH + 1];
+    tox_group_self_get_name(tox, groupnumber, (uint8_t *) self_name, &s_err);
+    self_name[self_name_len] = 0;
+
+    ck_assert(s_err == TOX_ERR_GROUP_SELF_QUERY_OK);
+    ck_assert(memcmp(self_name, PEER0_NICK, self_name_len) == 0);
+
+    printf("%s sent private action to %s: %s\n", peer_name, self_name, message_buf);
+    ck_assert(memcmp(message_buf, TEST_PRIVATE_MESSAGE, length) == 0);
+
+    State *state = (State *)user_data;
+    ck_assert(type == TOX_MESSAGE_TYPE_ACTION);
+    state->private_message_received = true;
+}
+
 static void group_message_handler_2(Tox *tox, uint32_t groupnumber, uint32_t peer_id, TOX_MESSAGE_TYPE type,
                                     const uint8_t *message, size_t length, void *user_data)
 {
     State *state = (State *)user_data;
+
     ck_assert(state != nullptr);
     ck_assert(length > 0 && length <= TOX_MAX_MESSAGE_LENGTH);
 
@@ -131,12 +229,16 @@ static void group_message_test(Tox **toxes, State *state)
     tox_callback_group_invite(toxes[1], group_invite_handler);
     tox_callback_group_join_fail(toxes[1], group_join_fail_handler);
     tox_callback_group_peer_join(toxes[1], group_peer_join_handler);
+    tox_callback_group_join_fail(toxes[0], group_join_fail_handler);
+    tox_callback_group_peer_join(toxes[0], group_peer_join_handler);
     tox_callback_group_message(toxes[0], group_message_handler);
+    tox_callback_group_custom_packet(toxes[1], group_custom_packet_handler);
+    tox_callback_group_private_message(toxes[1], group_private_message_handler);
 
     // tox0 makes new group.
     TOX_ERR_GROUP_NEW err_new;
     uint32_t group_number = tox_group_new(toxes[0], TOX_GROUP_PRIVACY_STATE_PRIVATE, (const uint8_t *)TEST_GROUP_NAME,
-                                          strlen(TEST_GROUP_NAME), (const uint8_t *)PEER1_NICK, strlen(PEER1_NICK), &err_new);
+                                          TEST_GROUP_NAME_LEN, (const uint8_t *)PEER1_NICK, PEER1_NICK_LEN, &err_new);
 
     ck_assert(err_new == TOX_ERR_GROUP_NEW_OK);
 
@@ -151,10 +253,34 @@ static void group_message_test(Tox **toxes, State *state)
         if (state[1].peer_joined && !state[1].message_sent) {
             TOX_ERR_GROUP_SEND_MESSAGE err_send;
             tox_group_send_message(toxes[1], group_number, TOX_MESSAGE_TYPE_NORMAL, (const uint8_t *)TEST_MESSAGE,
-                                   strlen(TEST_MESSAGE), &err_send);
+                                   TEST_MESSAGE_LEN, &err_send);
             ck_assert(err_send == TOX_ERR_GROUP_SEND_MESSAGE_OK);
             state[1].message_sent = true;
         }
+    }
+
+    fprintf(stderr, "Sending private message...\n");
+
+    // tox0 sends a private action to tox1
+    TOX_ERR_GROUP_SEND_PRIVATE_MESSAGE m_err;
+    tox_group_send_private_message(toxes[0], group_number, state[0].peer_id, TOX_MESSAGE_TYPE_ACTION,
+                                   (const uint8_t *)TEST_PRIVATE_MESSAGE, TEST_PRIVATE_MESSAGE_LEN, &m_err);
+    ck_assert_msg(m_err == TOX_ERR_GROUP_SEND_PRIVATE_MESSAGE_OK, "%d", m_err);
+
+    fprintf(stderr, "Sending custom packets...\n");
+
+    // // tox0 sends a lossless and lossy custom packet to tox1
+    TOX_ERR_GROUP_SEND_CUSTOM_PACKET c_err;
+    tox_group_send_custom_packet(toxes[0], group_number, true, (const uint8_t *)TEST_CUSTOM_PACKET, TEST_CUSTOM_PACKET_LEN,
+                                 &c_err);
+    ck_assert_msg(c_err == TOX_ERR_GROUP_SEND_CUSTOM_PACKET_OK, "%d", c_err);
+
+    tox_group_send_custom_packet(toxes[0], group_number, false, (const uint8_t *)TEST_CUSTOM_PACKET, TEST_CUSTOM_PACKET_LEN,
+                                 &c_err);
+    ck_assert_msg(c_err == TOX_ERR_GROUP_SEND_CUSTOM_PACKET_OK, "%d", c_err);
+
+    while (!state[1].private_message_received && state[1].custom_packets_received < 2) {
+        iterate_all_wait(NUM_GROUP_TOXES, toxes, state, ITERATION_INTERVAL);
     }
 
     // tox0 spams messages to tox1
@@ -202,7 +328,13 @@ int main(void)
 
 #undef NUM_GROUP_TOXES
 #undef PEER1_NICK
+#undef PEER1_NICK_LEN
 #undef PEER0_NICK
+#undef PEER0_NICK_LEN
 #undef TEST_GROUP_NAME
+#undef TEST_GROUP_NAME_LEN
 #undef TEST_MESSAGE
+#undef TEST_MESSAGE_LEN
+#undef TEST_PRIVATE_MESSAGE_LEN
+#undef TEST_CUSTOM_PACKET_LEN
 #undef MAX_NUM_MESSAGES
