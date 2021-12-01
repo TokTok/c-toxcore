TokTok/toxcore

Remove recursive calling code

toxcore/DHT.c
@@ -753,23 +753,28 @@ static int client_or_ip_port_in_list(const Logger *log, const Mono_Time *mono_ti
 bool add_to_list(Node_format *nodes_list, uint32_t length, const uint8_t *pk, IP_Port ip_port,
                  const uint8_t *cmp_pk)
 {
+    bool ret = false;
+    uint8_t pk_new[CRYPTO_PUBLIC_KEY_SIZE];
+    memcpy(pk_new, pk, CRYPTO_PUBLIC_KEY_SIZE);
+    IP_Port ip_port_new = ip_port;
+
+    uint8_t pk_bak[CRYPTO_PUBLIC_KEY_SIZE];
+    IP_Port ip_port_bak;
     for (uint32_t i = 0; i < length; ++i) {
-        if (id_closest(cmp_pk, nodes_list[i].public_key, pk) == 2) {
-            uint8_t pk_bak[CRYPTO_PUBLIC_KEY_SIZE];
+        if (id_closest(cmp_pk, nodes_list[i].public_key, pk_new) == 2) {
             memcpy(pk_bak, nodes_list[i].public_key, CRYPTO_PUBLIC_KEY_SIZE);
-            const IP_Port ip_port_bak = nodes_list[i].ip_port;
-            memcpy(nodes_list[i].public_key, pk, CRYPTO_PUBLIC_KEY_SIZE);
-            nodes_list[i].ip_port = ip_port;
+            memcpy(nodes_list[i].public_key, pk_new, CRYPTO_PUBLIC_KEY_SIZE);
+            memcpy(pk_new, pk_bak, CRYPTO_PUBLIC_KEY_SIZE);
 
-            if (i != length - 1) {
-                add_to_list(nodes_list, length, pk_bak, ip_port_bak, cmp_pk);
-            }
+            ip_port_bak = nodes_list[i].ip_port;
+            nodes_list[i].ip_port = ip_port_new;
+            ip_port_new = ip_port_bak;
 
-            return true;
+            ret = true;
         }
     }
 
-    return false;
+    return ret;    
 }
 
 /* TODO(irungentoo): change this to 7 when done*/

TokTok/c-toxcore

toxcore/DHT.c
@@ -753,23 +753,25 @@ static int client_or_ip_port_in_list(const Logger *log, const Mono_Time *mono_ti
 bool add_to_list(Node_format *nodes_list, uint32_t length, const uint8_t *pk, IP_Port ip_port,
                  const uint8_t *cmp_pk)
 {
+    uint32_t index = length;
+    uint8_t pk_bak[CRYPTO_PUBLIC_KEY_SIZE];
+    memcpy(pk_bak, pk, CRYPTO_PUBLIC_KEY_SIZE);
+
     for (uint32_t i = 0; i < length; ++i) {
-        if (id_closest(cmp_pk, nodes_list[i].public_key, pk) == 2) {
-            uint8_t pk_bak[CRYPTO_PUBLIC_KEY_SIZE];
+        if (id_closest(cmp_pk, nodes_list[i].public_key, pk_bak) == 2) {
+            index = i;
             memcpy(pk_bak, nodes_list[i].public_key, CRYPTO_PUBLIC_KEY_SIZE);
-            const IP_Port ip_port_bak = nodes_list[i].ip_port;
-            memcpy(nodes_list[i].public_key, pk, CRYPTO_PUBLIC_KEY_SIZE);
-            nodes_list[i].ip_port = ip_port;
-
-            if (i != length - 1) {
-                add_to_list(nodes_list, length, pk_bak, ip_port_bak, cmp_pk);
-            }
-
-            return true;
         }
     }
 
-    return false;
+    if (index == length) {
+        return false;
+    }
+
+    memcpy(nodes_list[index].public_key, pk, CRYPTO_PUBLIC_KEY_SIZE);
+    nodes_list[index].ip_port = ip_port;
+
+    return true;
 }
 
 /* TODO(irungentoo): change this to 7 when done*/