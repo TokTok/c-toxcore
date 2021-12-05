a375f58ac78e815200f6be0433452ad2d5e814a5
sudden6 <sudden6@gmx.at>
2021-12-05 13:07:27 +0100

    add fuzzing harnesses
    Toxsave harness ported to libFuzzer interface.
    New harness for bootstrap phase.

---
+++ testing/fuzzing/bootstrap_harness.cc
@@ -0,0 +1,28 @@
+#include "../../toxcore/tox.h"
+#include "network_adapter.h"
+
+#include <cstring>
+#include <cassert>
+
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
+    network_adapter_init(Data, Size);
+
+    Tox_Err_New error_new;
+    Tox *tox = tox_new(NULL, &error_new);
+
+    if (tox == nullptr || error_new != TOX_ERR_NEW_OK) {
+        return 0;
+    }
+
+    uint8_t pub_key[TOX_PUBLIC_KEY_SIZE] = {0};
+
+    assert(tox_bootstrap(tox, "127.0.0.1", 12345, pub_key, nullptr));
+
+    for (uint32_t i = 0; i < 100; i++) {
+        tox_iterate(tox, nullptr);
+    }
+
+    tox_kill(tox);
+    return 0;  // Non-zero return values are reserved for future use.
+}
