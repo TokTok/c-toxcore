9eb5ffa4c3773dd4b4ad7484a628b03ee81488fe
Restyled.io <commits@restyled.io>
2021-12-19 10:38:47 +0000

    Restyled by clang-format

--- testing/fuzzing/bootstrap_harness.cc
+++ testing/fuzzing/bootstrap_harness.cc
@@ -1,29 +1,27 @@
+#include <cassert>
+#include <cstring>
+
 #include "../../toxcore/tox.h"
 #include "fuzz_adapter.h"
 
-#include <cstring>
-#include <cassert>
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
+  network_adapter_init(Data, Size);
 
+  Tox_Err_New error_new;
+  Tox *tox = tox_new(NULL, &error_new);
 
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
-{
-    network_adapter_init(Data, Size);
+  if (tox == nullptr || error_new != TOX_ERR_NEW_OK) {
+    return 0;
+  }
 
-    Tox_Err_New error_new;
-    Tox *tox = tox_new(NULL, &error_new);
+  uint8_t pub_key[TOX_PUBLIC_KEY_SIZE] = {0};
 
-    if (tox == nullptr || error_new != TOX_ERR_NEW_OK) {
-        return 0;
-    }
+  assert(tox_bootstrap(tox, "127.0.0.1", 12345, pub_key, nullptr));
 
-    uint8_t pub_key[TOX_PUBLIC_KEY_SIZE] = {0};
+  for (uint32_t i = 0; i < 100; i++) {
+    tox_iterate(tox, nullptr);
+  }
 
-    assert(tox_bootstrap(tox, "127.0.0.1", 12345, pub_key, nullptr));
-
-    for (uint32_t i = 0; i < 100; i++) {
-        tox_iterate(tox, nullptr);
-    }
-
-    tox_kill(tox);
-    return 0;  // Non-zero return values are reserved for future use.
+  tox_kill(tox);
+  return 0;  // Non-zero return values are reserved for future use.
 }
--- testing/fuzzing/toxsave_harness.cc
+++ testing/fuzzing/toxsave_harness.cc
@@ -1,28 +1,27 @@
 #include "../../toxcore/tox.h"
 
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
-{
-    Tox_Err_Options_New error_options;
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
+  Tox_Err_Options_New error_options;
 
-    struct Tox_Options *tox_options = tox_options_new(&error_options);
+  struct Tox_Options *tox_options = tox_options_new(&error_options);
 
-    if (error_options != TOX_ERR_OPTIONS_NEW_OK) {
-        return 0;
-    }
+  if (error_options != TOX_ERR_OPTIONS_NEW_OK) {
+    return 0;
+  }
 
-    if (tox_options == nullptr) {
-        return 0;
-    }
+  if (tox_options == nullptr) {
+    return 0;
+  }
 
-    // pass test data to Tox
-    tox_options_set_savedata_data(tox_options, Data, Size);
-    tox_options_set_savedata_type(tox_options, TOX_SAVEDATA_TYPE_TOX_SAVE);
+  // pass test data to Tox
+  tox_options_set_savedata_data(tox_options, Data, Size);
+  tox_options_set_savedata_type(tox_options, TOX_SAVEDATA_TYPE_TOX_SAVE);
 
-    Tox_Err_New error_new;
-    Tox *tox = tox_new(tox_options, &error_new);
+  Tox_Err_New error_new;
+  Tox *tox = tox_new(tox_options, &error_new);
 
-    tox_options_free(tox_options);
+  tox_options_free(tox_options);
 
-    tox_kill(tox);
-    return 0;  // Non-zero return values are reserved for future use.
+  tox_kill(tox);
+  return 0;  // Non-zero return values are reserved for future use.
 }
