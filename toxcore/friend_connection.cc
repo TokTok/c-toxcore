106593329f5a62a1384214e01571c191d8cf5a8f
Restyled.io <commits@restyled.io>
2021-12-21 15:45:46 +0000

    Restyled by clang-format

--- toxcore/friend_connection.cc
+++ toxcore/friend_connection.cc
@@ -2,14 +2,12 @@
 
 #include <gtest/gtest.h>
 
-namespace
-{
+namespace {
 
 // TODO(Jfreegman) make this useful or remove it after NGC is merged
-TEST(friend_connection, NullTest)
-{
-    (void)friend_conn_get_onion_friendnum;
-    (void)friend_conn_get_dht_ip_port;
+TEST(friend_connection, NullTest) {
+  (void)friend_conn_get_onion_friendnum;
+  (void)friend_conn_get_dht_ip_port;
 }
 
 }  // namespace
