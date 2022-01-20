cfa8ae06e1f1abbda6aa386b302647245f4f584a
jfreegman <jfreegman@gmail.com>
2022-01-19 22:45:19 -0500

    Fix bootstrap and tcp relay autotests
    We now use the appropriate nodes depending on whether or
    not the test net is being run

--- auto_tests/bootstrap_test.c
+++ auto_tests/bootstrap_test.c
@@ -3,6 +3,7 @@
 #include "../testing/misc_tools.h"
 #include "check_compat.h"
 
+#ifndef USE_TEST_NETWORK
 static uint8_t const key1[] = {
     0x02, 0x80, 0x7C, 0xF4, 0xF8, 0xBB, 0x8F, 0xB3,
     0x90, 0xCC, 0x37, 0x94, 0xBD, 0xF1, 0xE8, 0x44,
@@ -30,14 +31,14 @@ static uint8_t const key4[] = {
     0x22, 0xF7, 0xAE, 0x5B, 0x20, 0xA0, 0xAC, 0xED,
     0x29, 0x81, 0x76, 0x9F, 0xC5, 0xB4, 0x37, 0x25,
 };
-
-/* testnet node */
+#else
 static uint8_t const key5[] = {
     0x79, 0xCA, 0xDA, 0x49, 0x74, 0xB0, 0x92, 0x6F,
     0x28, 0x6F, 0x02, 0x5C, 0xD5, 0xFF, 0xDF, 0x3E,
     0x65, 0x4A, 0x37, 0x58, 0xC5, 0x3E, 0x02, 0x73,
     0xEC, 0xFC, 0x4D, 0x12, 0xC2, 0x1D, 0xCA, 0x48,
 };
+#endif  // USE_TEST_NETWORK
 
 int main(void)
 {
@@ -45,11 +46,14 @@ int main(void)
 
     Tox *tox_udp = tox_new_log(nullptr, nullptr, nullptr);
 
+#ifndef USE_TEST_NETWORK
     tox_bootstrap(tox_udp, "78.46.73.141", 33445, key1, nullptr);
     tox_bootstrap(tox_udp, "tox.initramfs.io", 33445, key2, nullptr);
     tox_bootstrap(tox_udp, "tox2.abilinski.com", 33445, key3, nullptr);
     tox_bootstrap(tox_udp, "tox.plastiras.org", 443, key4, nullptr);
+#else
     tox_bootstrap(tox_udp, "172.93.52.70", 33445, key5, nullptr);
+#endif  // USE_TEST_NETWORK
 
     printf("Waiting for connection");
 
--- auto_tests/tcp_relay_test.c
+++ auto_tests/tcp_relay_test.c
@@ -3,6 +3,7 @@
 #include "../testing/misc_tools.h"
 #include "check_compat.h"
 
+#ifndef USE_TEST_NETWORK
 static uint8_t const key1[] = {
     0x02, 0x80, 0x7C, 0xF4, 0xF8, 0xBB, 0x8F, 0xB3,
     0x90, 0xCC, 0x37, 0x94, 0xBD, 0xF1, 0xE8, 0x44,
@@ -30,14 +31,14 @@ static uint8_t const key4[] = {
     0x22, 0xF7, 0xAE, 0x5B, 0x20, 0xA0, 0xAC, 0xED,
     0x29, 0x81, 0x76, 0x9F, 0xC5, 0xB4, 0x37, 0x25,
 };
-
-// testnet node
+#else
 static uint8_t const key5[] = {
     0x79, 0xCA, 0xDA, 0x49, 0x74, 0xB0, 0x92, 0x6F,
     0x28, 0x6F, 0x02, 0x5C, 0xD5, 0xFF, 0xDF, 0x3E,
     0x65, 0x4A, 0x37, 0x58, 0xC5, 0x3E, 0x02, 0x73,
     0xEC, 0xFC, 0x4D, 0x12, 0xC2, 0x1D, 0xCA, 0x48,
 };
+#endif // USE_TEST_NETWORK
 
 int main(void)
 {
@@ -48,13 +49,14 @@ int main(void)
     Tox *tox_tcp = tox_new_log(opts, nullptr, nullptr);
     tox_options_free(opts);
 
+    Tox_Err_Bootstrap tcp_err;
+
+#ifndef USE_TEST_NETWORK
     tox_bootstrap(tox_tcp, "78.46.73.141", 33445, key1, nullptr);
     tox_bootstrap(tox_tcp, "tox.initramfs.io", 33445, key2, nullptr);
     tox_bootstrap(tox_tcp, "tox2.abilinski.com", 33445, key3, nullptr);
     tox_bootstrap(tox_tcp, "tox.plastiras.org", 443, key4, nullptr);
-    tox_bootstrap(tox_tcp, "172.93.52.70", 33445, key5, nullptr);
 
-    Tox_Err_Bootstrap tcp_err;
     tox_add_tcp_relay(tox_tcp, "78.46.73.141", 33445, key1, &tcp_err);
     ck_assert_msg(tcp_err == TOX_ERR_BOOTSTRAP_OK,
                   "attempting to add tcp relay 1 returned with an error: %d",
@@ -71,10 +73,13 @@ int main(void)
     ck_assert_msg(tcp_err == TOX_ERR_BOOTSTRAP_OK,
                   "attempting to add tcp relay 4 returned with an error: %d",
                   tcp_err);
+#else
+    tox_bootstrap(tox_tcp, "172.93.52.70", 33445, key5, nullptr);
     tox_add_tcp_relay(tox_tcp, "172.93.52.70", 33445, key5, &tcp_err);
     ck_assert_msg(tcp_err == TOX_ERR_BOOTSTRAP_OK,
                   "attempting to add tcp relay 4 returned with an error: %d",
                   tcp_err);
+#endif // USE_TEST_NETWORK
 
     printf("Waiting for connection");
 
