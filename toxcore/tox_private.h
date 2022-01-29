89a1a04a98a5910fc77d54470da456d8ec4bd478
jfreegman <jfreegman@gmail.com>
2022-01-29 13:53:33 -0500

    Fix some minor issues with dht api
    - Use network.c wrapper for ntohs and remove unnecessary system headers
    - Less nesting in tox_dht_get_nodes_response_handler()
    - Make dht_getnodes return a bool instead of int

--- toxcore/tox_private.h
+++ toxcore/tox_private.h
@@ -48,9 +48,6 @@ void *tox_get_av_object(const Tox *tox);
 
 
 
-typedef struct Tox_Dht_Node {
-    void *data;
-} Tox_Dht_Node;
 
 /**
  * The minimum size of an IP string buffer in bytes.
@@ -71,16 +68,18 @@ uint32_t tox_dht_node_public_key_size(void);
 //!TOKSTYLE+
 
 /**
- * @param dht_node A node received from a get nodes response. This node must be
- *   freed by the caller using the tox_dht_node_free function.
+ * @param public_key The node's public key.
+ * @param ip The node's IP address, represented as a null terminated string.
+ * @param port The node's port.
  */
-typedef void tox_dht_get_nodes_response_cb(Tox *tox, Tox_Dht_Node *dht_node, void *user_data);
+typedef void tox_dht_get_nodes_response_cb(Tox *tox, const uint8_t *public_key, const char *ip, uint16_t port,
+                                           void *user_data);
 
 
 /**
  * Set the callback for the `dht_get_nodes_response` event. Pass NULL to unset.
  *
- * This event is triggered when a get nodes response is received from a DHT peer.
+ * This event is triggered when a getnodes response is received from a DHT peer.
  */
 void tox_callback_dht_get_nodes_response(Tox *tox, tox_dht_get_nodes_response_cb *callback);
 
@@ -92,71 +91,46 @@ typedef enum Tox_Err_Dht_Get_Nodes {
     TOX_ERR_DHT_GET_NODES_OK,
 
     /**
+     * UDP is disabled in tox options; the DHT can only be queried when UDP is enabled.
+     */
+    TOX_ERR_DHT_GET_NODES_UDP_DISABLED,
+
+    /**
      * One of the arguments to the function was NULL when it was not expected.
      */
     TOX_ERR_DHT_GET_NODES_NULL,
 
     /**
-     * The get nodes request failed. This may occur if the packet failed to send
-     * or the node contained invalid data.
+     * The supplied port is invalid.
+     */
+    TOX_ERR_DHT_GET_NODES_BAD_PORT,
+
+    /**
+     * The supplied IP address is invalid.
+     */
+    TOX_ERR_DHT_GET_NODES_BAD_IP,
+
+    /**
+     * The getnodes request failed. This usually means the packet failed to send.
      */
     TOX_ERR_DHT_GET_NODES_FAIL,
 } Tox_Err_Dht_Get_Nodes;
 
 /**
- * This function sends a get nodes request to a DHT node for its peers that
- * are "close" to the passed public key according to the distance metric used
+ * This function sends a getnodes request to a DHT node for its peers that
+ * are "close" to the passed target public key according to the distance metric used
  * by the DHT implementation.
  *
- * @param dest_node The node that we're sending the request to. This node must
- *   have been obtained from a `dht_get_nodes_response` event.
- * @param public_key The public key that we wish to query. This key must be
+ * @param public_key The public key of the node that we wish to query. This key must be
  *   at least `TOX_DHT_NODE_PUBLIC_KEY_SIZE` bytes in length.
+ * @param ip A NULL terminated string representing the IP address of the node we wish to query.
+ * @param port The port of the node we wish to query.
+ * @param target_public_key The public key for which we want to find close nodes.
  *
  * @return true on success.
  */
-bool tox_dht_get_nodes(const Tox *tox, const Tox_Dht_Node *dest_node, const uint8_t *public_key,
-                       Tox_Err_Dht_Get_Nodes *error);
-
-/**
- * Return the port being used by a DHT node.
- *
- * @param dht_node The DHT node being queried. If this parameter is NULL, the
- *   function returns 0.
- */
-uint16_t tox_dht_node_get_port(const Tox_Dht_Node *dht_node);
-
-/**
- * Write a DHT node's public key to to a byte array.
- *
- * @param dht_node The DHT node being queried.
- * @param public_key The byte array being written to. This array must have room for
- *   at least TOX_DHT_NODE_PUBLIC_KEY_SIZE bytes.
- *
- * If either of these parameters are NULL, the function has no effect.
- */
-void tox_dht_node_get_public_key(const Tox_Dht_Node *dht_node, uint8_t *public_key);
-
-/**
- * Write a DHT node's IP address to a NULL terminated char array.
- *
- * @param dht_node The DHT node being queried. If this parameter is NULL, the
- *   function has no effect.
- * @param ip_str The char array being written to. This array must have room for
- *   at least TOX_DHT_NODE_IP_STRING_SIZE chars.
- *
- * @return the number of chars written to ip_str, not including the terminating
- *   NULL byte.
- */
-size_t tox_dht_node_get_ip_string(const Tox_Dht_Node *dht_node, char *ip_str, size_t length);
-
-/**
- * Frees all dynamically allocated memory associated a DHT node.
- *
- * @param dht_node The node being freed. If this parameter is NULL, the function
- *   has no effect.
- */
-void tox_dht_node_free(Tox_Dht_Node *dht_node);
+bool tox_dht_get_nodes(const Tox *tox, const uint8_t *public_key, const char *ip, uint16_t port,
+                       const uint8_t *target_public_key, Tox_Err_Dht_Get_Nodes *error);
 
 #ifdef __cplusplus
 }
