7030e74f7ebfd992a7c8cfde5c79dadf971c9a04
jfreegman <jfreegman@gmail.com>
2022-01-31 12:03:25 -0500

    Add DHT queries to private API
    This commit adds functionality for clients to interact with
    the DHT, sending getnodes requests to their peers and receiving
    nodes in getnodes responses.

--- toxcore/DHT.c
+++ toxcore/DHT.c
@@ -105,6 +105,8 @@ struct DHT {
 
     Node_format to_bootstrap[MAX_CLOSE_TO_BOOTSTRAP_NODES];
     unsigned int num_to_bootstrap;
+
+    dht_get_nodes_response_cb *get_nodes_response;
 };
 
 const uint8_t *dht_friend_public_key(const DHT_Friend *dht_friend)
@@ -1502,6 +1504,10 @@ static int handle_sendnodes_ipv6(void *object, IP_Port source, const uint8_t *pa
         if (ipport_isset(&plain_nodes[i].ip_port)) {
             ping_node_from_getnodes_ok(dht, plain_nodes[i].public_key, plain_nodes[i].ip_port);
             returnedip_ports(dht, plain_nodes[i].ip_port, plain_nodes[i].public_key, packet + 1);
+
+            if (dht->get_nodes_response) {
+                dht->get_nodes_response(dht, &plain_nodes[i], userdata);
+            }
         }
     }
 
@@ -2399,6 +2405,11 @@ static int cryptopacket_handle(void *object, IP_Port source, const uint8_t *pack
     }
 
     return 1;
+}
+
+void dht_callback_get_nodes_response(DHT *dht, dht_get_nodes_response_cb *function)
+{
+    dht->get_nodes_response = function;
 }
 
 /*----------------------------------------------------------------------------------*/
