4f34f9d81bda80a2c4cddd4edea038345ebcb9c9
iphydf <iphydf@users.noreply.github.com>
2022-02-08 19:15:37 +0000

    cleanup: Add more nonnull and nullable annotations.

--- toxcore/ccompat.h
+++ toxcore/ccompat.h
@@ -8,10 +8,11 @@
 #ifndef C_TOXCORE_TOXCORE_CCOMPAT_H
 #define C_TOXCORE_TOXCORE_CCOMPAT_H
 
-#include <stdbool.h>
 #include <stdint.h>
 
-bool unused_for_tokstyle(void);
+#include "attributes.h"
+
+/* No declarations here. */
 
 //!TOKSTYLE-
 
@@ -63,9 +64,13 @@ bool unused_for_tokstyle(void);
 
 #ifdef __GNUC__
 #define GNU_PRINTF(f, a) __attribute__((__format__(__printf__, f, a)))
-#define non_null(...) __attribute__((__nonnull__(__VA_ARGS__)))
 #else
 #define GNU_PRINTF(f, a)
+#endif
+
+#if defined(__GNUC__) && !defined(NDEBUG)
+#define non_null(...) __attribute__((__nonnull__(__VA_ARGS__)))
+#else
 #define non_null(...)
 #endif
 
