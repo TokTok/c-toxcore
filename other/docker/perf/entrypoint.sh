94cb6b719ca91acafed220beb5589ccc503996ed
Restyled.io <commits@restyled.io>
2022-02-18 21:11:49 +0000

    Restyled by shellharden

--- other/docker/perf/entrypoint.sh
+++ other/docker/perf/entrypoint.sh
@@ -7,12 +7,12 @@ OUTPUT="/work/c-toxcore/test.perf"
 
 gcc -pthread -g \
   -o "/work/$TEST" -O3 -fno-omit-frame-pointer \
-  $(find /work/c-toxcore/tox* -name "*.c") \
+  "$(find /work/c-toxcore/tox* -name "*.c")" \
   /work/c-toxcore/auto_tests/auto_test_support.c \
   /work/c-toxcore/testing/misc_tools.c \
   "/work/c-toxcore/auto_tests/$TEST.c" \
   -DMIN_LOGGER_LEVEL=LOGGER_LEVEL_TRACE \
-  $(pkg-config --cflags --libs libsodium msgpack opus vpx)
+  "$(pkg-config --cflags --libs libsodium msgpack opus vpx)"
 
 time perf record -g --call-graph dwarf --freq=999 "/work/$TEST" /work/c-toxcore/auto_tests/
 perf report | head -n50
