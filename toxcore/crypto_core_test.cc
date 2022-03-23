39db5e78e7d2b0683c9eef703168d31af4560411
jfreegman <jfreegman@gmail.com>
2022-03-22 22:56:07 -0400

    Merge moderation portion of new groupchats codebase

--- toxcore/crypto_core_test.cc
+++ toxcore/crypto_core_test.cc
@@ -1,4 +1,5 @@
 #include "crypto_core.h"
+#include "util.h"
 
 #include <gtest/gtest.h>
 
@@ -6,41 +7,63 @@
 #include <array>
 #include <vector>
 
-namespace {
+namespace
+{
+
+using ExtPublicKey = std::array<uint8_t, EXT_PUBLIC_KEY_SIZE>;
+using ExtSecretKey = std::array<uint8_t, EXT_SECRET_KEY_SIZE>;
+using Signature = std::array<uint8_t, CRYPTO_SIGNATURE_SIZE>;
+using Nonce = std::array<uint8_t, CRYPTO_NONCE_SIZE>;
 
 TEST(CryptoCore, IncrementNonce)
 {
-    using Nonce = std::array<uint8_t, CRYPTO_NONCE_SIZE>;
     Nonce nonce{};
     increment_nonce(nonce.data());
     EXPECT_EQ(
-        nonce, (Nonce{{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}}));
+    nonce, (Nonce{{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}}));
 
     for (int i = 0; i < 0x1F4; ++i) {
         increment_nonce(nonce.data());
     }
 
     EXPECT_EQ(nonce,
-        (Nonce{{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x01, 0xF5}}));
+    (Nonce{{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x01, 0xF5}}));
 }
 
 TEST(CryptoCore, IncrementNonceNumber)
 {
-    using Nonce = std::array<uint8_t, CRYPTO_NONCE_SIZE>;
     Nonce nonce{};
 
     increment_nonce_number(nonce.data(), 0x1F5);
     EXPECT_EQ(nonce,
-        (Nonce{{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x01, 0xF5}}));
+    (Nonce{{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x01, 0xF5}}));
 
     increment_nonce_number(nonce.data(), 0x1F5);
     EXPECT_EQ(nonce,
-        (Nonce{{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x03, 0xEA}}));
+    (Nonce{{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x03, 0xEA}}));
 
     increment_nonce_number(nonce.data(), 0x12345678);
     EXPECT_EQ(nonce,
-        (Nonce{
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x12, 0x34, 0x5A, 0x62}}));
+    (Nonce{
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x12, 0x34, 0x5A, 0x62}}));
+}
+
+TEST(CryptoCore, Signatures)
+{
+    ExtPublicKey pk;
+    ExtSecretKey sk;
+
+    EXPECT_TRUE(create_extended_keypair(pk.data(), sk.data()));
+
+    std::vector<uint8_t> message;
+
+    // Try a few different sizes, including empty 0 length message.
+    for (uint8_t i = 0; i < 100; ++i) {
+        message.push_back(random_u08());
+        Signature signature;
+        EXPECT_TRUE(crypto_signature_create(signature.data(), message.data(), message.size(), get_sig_sk(sk.data())));
+        EXPECT_TRUE(crypto_signature_verify(signature.data(), message.data(), message.size(), get_sig_pk(pk.data())));
+    }
 }
 
 }  // namespace
---
+++ toxcore/group_moderation_test.cc
@@ -0,0 +1,119 @@
+#include "group_moderation.h"
+
+#include <gtest/gtest.h>
+
+#include <algorithm>
+#include <array>
+#include <vector>
+
+#include "crypto_core.h"
+
+namespace
+{
+
+using ModerationHash = std::array<uint8_t, MOD_MODERATION_HASH_SIZE>;
+
+TEST(ModList, PackedSizeOfEmptyModListIsZero)
+{
+    Moderation mods{};
+    EXPECT_EQ(mod_list_packed_size(&mods), 0);
+
+    uint8_t byte = 1;
+    mod_list_pack(&mods, &byte);
+    EXPECT_EQ(byte, 1);
+}
+
+TEST(ModList, UnpackingZeroSizeArrayIsNoop)
+{
+    Moderation mods{};
+    const uint8_t byte = 1;
+    EXPECT_EQ(mod_list_unpack(&mods, &byte, 0, 0), 0);
+}
+
+TEST(ModList, PackingAndUnpackingList)
+{
+    using ModListEntry = std::array<uint8_t, MOD_LIST_ENTRY_SIZE>;
+    Moderation mods{};
+    EXPECT_TRUE(mod_list_add_entry(&mods, ModListEntry{}.data()));
+
+    std::vector<uint8_t> packed(mod_list_packed_size(&mods));
+    mod_list_pack(&mods, packed.data());
+
+    Moderation mods2{};
+    EXPECT_EQ(mod_list_unpack(&mods2, packed.data(), packed.size(), 1), packed.size());
+}
+
+TEST(ModList, UnpackingTooManyModsFails)
+{
+    using ModListEntry = std::array<uint8_t, MOD_LIST_ENTRY_SIZE>;
+    Moderation mods{};
+    EXPECT_TRUE(mod_list_add_entry(&mods, ModListEntry{}.data()));
+
+    std::vector<uint8_t> packed(mod_list_packed_size(&mods));
+    mod_list_pack(&mods, packed.data());
+
+    Moderation mods2{};
+    EXPECT_EQ(mod_list_unpack(&mods2, packed.data(), packed.size(), 2), -1);
+}
+
+TEST(ModList, UnpackingFromEmptyBufferFails)
+{
+    std::vector<uint8_t> packed(1);
+
+    Moderation mods{};
+    EXPECT_EQ(mod_list_unpack(&mods, packed.end().base(), 0, 1), -1);
+}
+
+TEST(ModList, HashOfEmptyModListZeroesOutBuffer)
+{
+    Moderation mods{};
+
+    // Fill with random data, check that it's zeroed.
+    ModerationHash hash;
+    random_bytes(hash.data(), hash.size());
+    EXPECT_TRUE(mod_list_make_hash(&mods, hash.data()));
+    EXPECT_EQ(hash, ModerationHash{});
+}
+
+TEST(ModList, RemoveIndexFromEmptyModListFails)
+{
+    Moderation mods{};
+    EXPECT_FALSE(mod_list_remove_index(&mods, 0));
+    EXPECT_FALSE(mod_list_remove_index(&mods, UINT16_MAX));
+}
+
+TEST(ModList, RemoveEntryFromEmptyModListFails)
+{
+    Moderation mods{};
+    uint8_t sig_pk[32] = {0};
+    EXPECT_FALSE(mod_list_remove_entry(&mods, sig_pk));
+}
+
+TEST(ModList, CleanupOnEmptyModsIsNoop)
+{
+    Moderation mods{};
+    mod_list_cleanup(&mods);
+}
+
+TEST(ModList, EmptyModListCannotVerifyAnySigPk)
+{
+    Moderation mods{};
+    uint8_t sig_pk[32] = {0};
+    EXPECT_FALSE(mod_list_verify_sig_pk(&mods, sig_pk));
+}
+
+TEST(SanctionsList, PackingEmptyListsIsNoop)
+{
+    std::array<uint8_t, 1> packed{0x7f};
+    EXPECT_EQ(sanctions_list_pack(packed.data(), packed.size(), nullptr, nullptr, 0), 0);
+    EXPECT_EQ(packed[0], 0x7f);
+}
+
+TEST(SanctionsList, PackingIntoUndersizedBufferFails)
+{
+    Mod_Sanction sanctions[1];
+    std::array<uint8_t, 1> packed;
+    EXPECT_EQ(sanctions_list_pack(packed.data(), packed.size(), sanctions, nullptr, 1), -1);
+}
+
+}  // namespace
