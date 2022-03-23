04d5857407f6c037ff57628ee49d998843e07197
iphydf <iphydf@users.noreply.github.com>
2022-03-23 00:46:10 +0000

    test: Add fuzzer tests to the bazel build.

---
+++ toxcore/DHT_fuzz_test.cc
@@ -0,0 +1,69 @@
+#include "DHT.h"
+
+#include <cassert>
+#include <vector>
+
+namespace {
+
+void TestHandleRequest(const uint8_t *data, size_t size)
+{
+    const uint8_t *self_public_key = data;
+    data += CRYPTO_PUBLIC_KEY_SIZE;
+    size -= CRYPTO_PUBLIC_KEY_SIZE;
+
+    const uint8_t *self_secret_key = data;
+    data += CRYPTO_SECRET_KEY_SIZE;
+    size -= CRYPTO_SECRET_KEY_SIZE;
+
+    uint8_t public_key[CRYPTO_PUBLIC_KEY_SIZE];
+    uint8_t request[MAX_CRYPTO_REQUEST_SIZE];
+    uint8_t request_id;
+    handle_request(self_public_key, self_secret_key, public_key, request, &request_id, data, size);
+}
+
+void TestUnpackNodes(const uint8_t *data, size_t size)
+{
+    if (size < 1) {
+        return;
+    }
+
+    const bool tcp_enabled = data[0];
+    ++data;
+    --size;
+
+    Node_format nodes[5];
+    uint16_t processed_data_len;
+    const int packed_count = unpack_nodes(nodes, 5, &processed_data_len, data, size, tcp_enabled);
+    if (packed_count > 0) {
+        Logger *logger = logger_new();
+        std::vector<uint8_t> packed(packed_count * PACKED_NODE_SIZE_IP6);
+        const int packed_size = pack_nodes(logger, packed.data(), packed.size(), nodes, packed_count);
+        assert(packed_size == processed_data_len);
+        logger_kill(logger);
+    }
+}
+
+}  // namespace
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size);
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+    if (size < 1) {
+        return 0;
+    }
+
+    const uint8_t func = data[0];
+    ++data;
+    --size;
+
+    switch (func) {
+    case 0:
+        TestHandleRequest(data, size);
+        return 0;
+    case 1:
+        TestUnpackNodes(data, size);
+        return 0;
+    default:
+        return 0;
+    }
+}
