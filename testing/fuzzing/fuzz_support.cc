dd6685d6a3f7ed35e8241ecd0395951beff3ad32
Restyled.io <commits@restyled.io>
2022-03-29 22:40:59 +0000

    Restyled by clang-format

--- testing/fuzzing/fuzz_support.cc
+++ testing/fuzzing/fuzz_support.cc
@@ -10,6 +10,6 @@ std::unique_ptr<Tox_System> fuzz_system(uint64_t *clock)
     auto sys = std::make_unique<Tox_System>();
     sys->mono_time_callback = [](void *user_data) { return *static_cast<uint64_t *>(user_data); };
     sys->mono_time_user_data = clock;
-    sys->ns = system_network(); // TODO(iphydf): Put fuzz_network here.
+    sys->ns = system_network();  // TODO(iphydf): Put fuzz_network here.
     return sys;
 }
