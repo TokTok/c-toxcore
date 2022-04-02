3729dad1e62ba03549ed71579cb4fd04938d003d
iphydf <iphydf@users.noreply.github.com>
2022-04-02 17:10:09 +0000

    test: Add more functionality to the bootstrap harness.
    Ideally this would be able to reach some of the events, so we can write
    code to respond to those events, but so far only the friend request
    event actually happens.

--- testing/fuzzing/bootstrap_harness.cc
+++ testing/fuzzing/bootstrap_harness.cc
@@ -3,19 +3,131 @@
 #include <memory>
 
 #include "../../toxcore/tox.h"
+#include "../../toxcore/tox_dispatch.h"
+#include "../../toxcore/tox_events.h"
 #include "../../toxcore/tox_private.h"
+#include "../../toxcore/tox_struct.h"
+#include "../../toxcore/util.h"
 #include "fuzz_adapter.h"
 #include "fuzz_support.h"
+
+namespace {
+
+void setup_callbacks(Tox_Dispatch *dispatch)
+{
+    tox_events_callback_conference_connected(
+        dispatch, [](Tox *tox, const Tox_Event_Conference_Connected *event, void *user_data) {
+            assert(event == nullptr);
+        });
+    tox_events_callback_conference_connected(
+        dispatch, [](Tox *tox, const Tox_Event_Conference_Connected *event, void *user_data) {
+            assert(event == nullptr);
+        });
+    tox_events_callback_conference_invite(
+        dispatch, [](Tox *tox, const Tox_Event_Conference_Invite *event, void *user_data) {
+            assert(event == nullptr);
+        });
+    tox_events_callback_conference_message(
+        dispatch, [](Tox *tox, const Tox_Event_Conference_Message *event, void *user_data) {
+            assert(event == nullptr);
+        });
+    tox_events_callback_conference_peer_list_changed(dispatch,
+        [](Tox *tox, const Tox_Event_Conference_Peer_List_Changed *event, void *user_data) {
+            assert(event == nullptr);
+        });
+    tox_events_callback_conference_peer_name(
+        dispatch, [](Tox *tox, const Tox_Event_Conference_Peer_Name *event, void *user_data) {
+            assert(event == nullptr);
+        });
+    tox_events_callback_conference_title(
+        dispatch, [](Tox *tox, const Tox_Event_Conference_Title *event, void *user_data) {
+            assert(event == nullptr);
+        });
+    tox_events_callback_file_chunk_request(
+        dispatch, [](Tox *tox, const Tox_Event_File_Chunk_Request *event, void *user_data) {
+            assert(event == nullptr);
+        });
+    tox_events_callback_file_recv(
+        dispatch, [](Tox *tox, const Tox_Event_File_Recv *event, void *user_data) {
+            assert(event == nullptr);
+        });
+    tox_events_callback_file_recv_chunk(
+        dispatch, [](Tox *tox, const Tox_Event_File_Recv_Chunk *event, void *user_data) {
+            assert(event == nullptr);
+        });
+    tox_events_callback_file_recv_control(
+        dispatch, [](Tox *tox, const Tox_Event_File_Recv_Control *event, void *user_data) {
+            assert(event == nullptr);
+        });
+    tox_events_callback_friend_connection_status(
+        dispatch, [](Tox *tox, const Tox_Event_Friend_Connection_Status *event, void *user_data) {
+            assert(event == nullptr);
+        });
+    tox_events_callback_friend_lossless_packet(
+        dispatch, [](Tox *tox, const Tox_Event_Friend_Lossless_Packet *event, void *user_data) {
+            assert(event == nullptr);
+        });
+    tox_events_callback_friend_lossy_packet(
+        dispatch, [](Tox *tox, const Tox_Event_Friend_Lossy_Packet *event, void *user_data) {
+            assert(event == nullptr);
+        });
+    tox_events_callback_friend_message(
+        dispatch, [](Tox *tox, const Tox_Event_Friend_Message *event, void *user_data) {
+            assert(event == nullptr);
+        });
+    tox_events_callback_friend_name(
+        dispatch, [](Tox *tox, const Tox_Event_Friend_Name *event, void *user_data) {
+            assert(event == nullptr);
+        });
+    tox_events_callback_friend_read_receipt(
+        dispatch, [](Tox *tox, const Tox_Event_Friend_Read_Receipt *event, void *user_data) {
+            assert(event == nullptr);
+        });
+    tox_events_callback_friend_request(
+        dispatch, [](Tox *tox, const Tox_Event_Friend_Request *event, void *user_data) {
+            Tox_Err_Friend_Add err;
+            tox_friend_add_norequest(tox, tox_event_friend_request_get_public_key(event), &err);
+            assert(err == TOX_ERR_FRIEND_ADD_OK || err == TOX_ERR_FRIEND_ADD_OWN_KEY
+                || err == TOX_ERR_FRIEND_ADD_ALREADY_SENT
+                || err == TOX_ERR_FRIEND_ADD_BAD_CHECKSUM);
+        });
+    tox_events_callback_friend_status(
+        dispatch, [](Tox *tox, const Tox_Event_Friend_Status *event, void *user_data) {
+            assert(event == nullptr);
+        });
+    tox_events_callback_friend_status_message(
+        dispatch, [](Tox *tox, const Tox_Event_Friend_Status_Message *event, void *user_data) {
+            assert(event == nullptr);
+        });
+    tox_events_callback_friend_typing(
+        dispatch, [](Tox *tox, const Tox_Event_Friend_Typing *event, void *user_data) {
+            assert(event == nullptr);
+        });
+    tox_events_callback_self_connection_status(
+        dispatch, [](Tox *tox, const Tox_Event_Self_Connection_Status *event, void *user_data) {
+            assert(event == nullptr);
+        });
+}
+
+}
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size);
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
+    if (size < 1) {
+        return 0;
+    }
+    const uint8_t iterations = data[0];
+    ++data;
+    --size;
+
     network_adapter_init(data, size);
 
     uint64_t clock = 0;
     auto sys = fuzz_system(clock);
     assert(sys->mono_time_callback != nullptr);
     assert(sys->mono_time_user_data != nullptr);
+    assert(sys->rng != nullptr);
 
     Tox_Options *opts = tox_options_new(nullptr);
     assert(opts != nullptr);
@@ -34,19 +146,25 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
     const bool success = tox_bootstrap(tox, "127.0.0.1", 12345, pub_key, nullptr);
     assert(success);
 
-    /*
-     * The iteration count here is a magic value in the literal sense, too small
-     * and coverage will be bad, too big and fuzzing will not be efficient.
-     * NOTE: This should be fine tuned after gathering some experience.
-     */
+    tox_events_init(tox);
 
-    for (uint32_t i = 0; i < 50; ++i) {
-        tox_iterate(tox, nullptr);
-        // Move the clock forward a decent amount so all the time-based checks
-        // trigger more quickly.
-        clock += 200;
+    Tox_Dispatch *dispatch = tox_dispatch_new(nullptr);
+    assert(dispatch != nullptr);
+    setup_callbacks(dispatch);
+
+    for (uint32_t i = 0; i < iterations; ++i) {
+        Tox_Err_Events_Iterate error_iterate;
+        Tox_Events *events = tox_events_iterate(tox, true, &error_iterate);
+        assert(tox_events_equal(events, events));
+        tox_dispatch_invoke(dispatch, events, tox, nullptr);
+        tox_events_free(events);
+        // Move the clock forward a random amount so all the time-based checks
+        // trigger more quickly. Move by at least 20ms (iteration interval) so
+        // at least something happens.
+        clock += max_u16(20, random_u08(sys->rng));
     }
 
+    tox_dispatch_free(dispatch);
     tox_kill(tox);
     return 0;  // Non-zero return values are reserved for future use.
 }
