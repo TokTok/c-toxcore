98036eb2c617a39c64e1f5baf4dfef7720e4cfbe
robert y <robert@freylax.de>
2023-12-29 01:46:36 +0100

    new dependency framework, added more modules and tests

--- build.zig
+++ build.zig
@@ -2,7 +2,10 @@ const std = @import("std");
 const fmt = std.fmt;
 const heap = std.heap;
 const fs = std.fs;
-//const mem = std.mem;
+const Build = std.Build;
+const Step = Build.Step;
+const Compile = Step.Compile;
+const trimRight = std.mem.trimRight;
 const LibExeObjStep = std.build.LibExeObjStep;
 const Target = std.Target;
 
@@ -22,12 +25,95 @@ fn ensureDependencySubmodule(allocator: std.mem.Allocator, path: []const u8) !vo
     _ = try child.spawnAndWait();
 }
 
+const Relief = struct {
+    b: *Build,
+    target: std.zig.CrossTarget,
+    optimize: std.builtin.OptimizeMode,
+    subdir: []const u8,
+    gtest_lib: *Compile,
+    gtest_main: *Compile,
+
+    fn inSubdir(self: Relief, file: []const u8) []const u8 {
+        const p = self.b.pathJoin(&.{ self.subdir, file });
+        //std.debug.print("in subdir:{s},{s},{s}\n", .{ self.subdir, file, p });
+        return p[0..];
+    }
+    pub const Options = struct {
+        name: []const u8 = "", // if empty then the stem of first c file
+        c: []const u8 = "", // if not empty then only one c file
+        cs: []const []const u8 = &.{}, // possibly multiple c files
+        libs: []const *Compile = &.{},
+    };
+    // this two kind of options is to have an easy input for the common case
+    const Opt = struct {
+        name: []const u8,
+        cs: []const []const u8,
+        libs: []const *Compile,
+        fn init(b: *Build, o: Options) Opt {
+            const cs = if (o.c.len > 0) b.dupeStrings(&.{o.c}) else o.cs;
+            const name =
+                if (o.name.len > 0) o.name else if (cs.len > 0) trimRight(u8, cs[0], ".c") else "noname";
+            // std.debug.print("name = {s}, {s}\n", .{ name, cs[0] });
+            return Opt{ .name = name, .cs = cs, .libs = o.libs };
+        }
+    };
+    // add object
+    fn addObj(self: Relief, op: Options) [1]*Compile {
+        const o = Opt.init(self.b, op);
+        const c = self.b.addObject(
+            .{ .name = o.name, .target = self.target, .optimize = self.optimize },
+        );
+        c.linkLibC();
+        c.addIncludePath(.{ .path = self.subdir });
+        for (o.cs) |file| {
+            c.addCSourceFile(
+                .{ .file = .{ .path = self.inSubdir(file) }, .flags = &.{} },
+            );
+        }
+        for (o.libs) |l| {
+            c.installLibraryHeaders(l);
+            c.linkLibrary(l);
+        }
+        return [_]*Compile{c};
+    }
+    fn addGTest(self: Relief, op: Options, d: []const *Compile) *Compile {
+        const o = Opt.init(self.b, op);
+        const c = self.b.addExecutable(.{ .name = o.name });
+        for (o.cs) |file| {
+            c.addCSourceFile(
+                .{ .file = .{ .path = self.inSubdir(file) }, .flags = &.{} },
+            );
+        }
+        // we ommit duplicate dependencies
+        for (0..d.len) |i| {
+            var unique = true;
+            for (0..i) |j| {
+                if (j < i and d[i] == d[j]) {
+                    unique = false;
+                    break;
+                }
+            }
+            if (unique) c.addObject(d[i]);
+        }
+        for (o.libs) |l| {
+            c.installLibraryHeaders(l);
+            c.linkLibrary(l);
+        }
+        c.installLibraryHeaders(self.gtest_lib);
+        c.linkLibrary(self.gtest_lib);
+        c.linkLibrary(self.gtest_main);
+        c.linkLibC();
+        self.b.installArtifact(c);
+        return c;
+    }
+};
+
 pub fn build(b: *std.build.Builder) !void {
     const root_path = b.pathFromRoot(".");
     var cwd = try fs.openDirAbsolute(root_path, .{});
     defer cwd.close();
 
-    var target = b.standardTargetOptions(.{});
+    const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
     const build_static = b.option(bool, "static", "Build libtoxcore as a static library.") orelse true;
@@ -48,6 +134,7 @@ pub fn build(b: *std.build.Builder) !void {
         "libsodium",
         .{ .target = target, .optimize = optimize, .static = true, .shared = false },
     );
+    const libsodium = libsodium_dep.artifact("sodium");
 
     const gtest_dep = b.dependency(
         "gtest",
@@ -56,7 +143,17 @@ pub fn build(b: *std.build.Builder) !void {
             .optimize = optimize,
         },
     );
+
     ensureDependencySubmodule(b.allocator, "third_party/cmp") catch unreachable;
+
+    const r = Relief{
+        .b = b,
+        .target = target,
+        .optimize = optimize,
+        .subdir = "toxcore",
+        .gtest_lib = gtest_dep.artifact("gtest"),
+        .gtest_main = gtest_dep.artifact("gtest-main"),
+    };
 
     // work out which libraries we are building
     var libs = std.ArrayList(*LibExeObjStep).init(b.allocator);
@@ -81,7 +178,7 @@ pub fn build(b: *std.build.Builder) !void {
         lib.installHeader("toxcore/tox.h", "tox.h");
         lib.linkLibC();
         lib.installHeadersDirectory("toxcore", "toxcore");
-        lib.linkLibrary(libsodium_dep.artifact("sodium"));
+        lib.linkLibrary(libsodium);
 
         const allocator = heap.page_allocator;
         const src_path = "toxcore";
@@ -114,20 +211,59 @@ pub fn build(b: *std.build.Builder) !void {
     toxcore_zig_step.dependOn(&toxcore_zig.step);
     toxcore_zig_step.dependOn(&b.addInstallFile(toxcore_zig_file, "toxcore.zig").step);
 
-    // build tests
-    const gtest_lib = gtest_dep.artifact("gtest");
-    const gtest_main = gtest_dep.artifact("gtest-main");
-
-    const mem = b.addObject(.{ .name = "mem", .target = target, .optimize = optimize });
-    mem.addCSourceFile(.{ .file = .{ .path = "toxcore/mem.c" }, .flags = &.{} });
-    mem.addIncludePath(.{ .path = "toxcore" });
-    mem.linkLibC();
-    const mem_test = b.addExecutable(.{ .name = "mem_test" });
-    mem_test.addCSourceFile(.{ .file = .{ .path = "toxcore/mem_test.cc" }, .flags = &.{} });
-    mem_test.addObject(mem);
-    mem_test.installLibraryHeaders(gtest_lib);
-    mem_test.linkLibrary(gtest_lib);
-    mem_test.linkLibrary(gtest_main);
-    mem_test.linkLibC();
-    b.installArtifact(mem_test);
+    const mem = r.addObj(.{ .c = "mem.c" });
+    const util = r.addObj(.{ .c = "util.c" }) ++ mem;
+    const logger = r.addObj(.{ .c = "logger.c" });
+    const cmp = r.addObj(.{ .name = "cmp", .c = "../third_party/cmp/cmp.c" });
+    const bin_pack = r.addObj(.{ .c = "bin_pack.c" }) ++ logger ++ cmp;
+    const bin_unpack = r.addObj(.{ .c = "bin_unpack.c" }) ++ cmp;
+    const crypto_core = r.addObj(.{ .c = "crypto_core.c", .libs = &.{libsodium} });
+    const list = r.addObj(.{ .c = "list.c" });
+    const state = r.addObj(.{ .c = "state.c" });
+    const mono_time = r.addObj(.{ .c = "mono_time.c" }) ++ mem ++ util;
+    const shared_key_cache = r.addObj(.{ .c = "shared_key_cache.c" }) ++ crypto_core ++ logger ++ mem ++ mono_time;
+    const network = r.addObj(.{ .c = "network.c", .libs = &.{libsodium} }) ++ crypto_core ++ logger ++ mem ++ mono_time ++ util;
+    const timed_auth = r.addObj(.{ .c = "timed_auth.c" }) ++ crypto_core ++ mono_time;
+    const ping_array = r.addObj(.{ .c = "ping_array.c" }) ++ crypto_core ++ mem ++ mono_time ++ util;
+    const LAN_discovery = r.addObj(.{ .c = "LAN_discovery.c" }) ++ network;
+    const DHT = r.addObj(.{ .cs = &.{ "DHT.c", "ping.c" } }) ++ bin_pack ++ network ++ ping_array ++ LAN_discovery ++ shared_key_cache ++ state;
+    const onion = r.addObj(.{ .c = "onion.c" }) ++ DHT;
+    const forwarding = r.addObj(.{ .c = "forwarding.c" }) ++ network ++ timed_auth;
+    const announce = r.addObj(.{ .c = "announce.c" }) ++ LAN_discovery ++ forwarding ++ shared_key_cache;
+    const TCP_common = r.addObj(.{ .c = "TCP_common.c" }) ++ network;
+    // TODO     copts = select({
+    // "//tools/config:linux": ["-DTCP_SERVER_USE_EPOLL=1"],
+    //    "//conditions:default": [],
+    //}),
+    const TCP_server = r.addObj(.{ .c = "TCP_server.c" }) ++ TCP_common ++ crypto_core ++ forwarding ++ list ++ mono_time ++ onion;
+    const TCP_client = r.addObj(.{ .c = "TCP_client.c" }) ++ TCP_common ++ crypto_core ++ forwarding;
+    const TCP_connection = r.addObj(.{ .c = "TCP_connection.c" }) ++ TCP_client;
+    const net_crypto = r.addObj(.{ .c = "net_crypto.c" }) ++ DHT ++ TCP_connection ++ list;
+    const onion_announce = r.addObj(.{ .c = "onion_announce.c" }) ++ DHT ++ onion;
+    const group_announce = r.addObj(.{ .c = "group_announce.c" }) ++ DHT;
+    const group_onion_announce = r.addObj(.{ .c = "group_announce.c" }) ++ group_announce ++ onion_announce;
+    const onion_client = r.addObj(.{ .c = "onion_client.c" }) ++ group_onion_announce;
+    const friend_connection = r.addObj(.{ .c = "friend_connection.c" }) ++ net_crypto ++ onion_client;
+    _ = TCP_server;
+    //_ = TCP_client;
+    _ = announce;
+    //_ = onion_announce;
+    _ = friend_connection;
+    // tests
+    _ = r.addGTest(.{ .c = "mem_test.cc" }, &mem);
+    _ = r.addGTest(.{ .c = "util_test.cc" }, &(util ++ crypto_core));
+    _ = r.addGTest(.{ .c = "bin_pack_test.cc" }, &(bin_pack ++ bin_unpack));
+    _ = r.addGTest(.{ .c = "crypto_core_test.cc" }, &crypto_core);
+    _ = r.addGTest(.{ .c = "list_test.cc" }, &list);
+    _ = r.addGTest(.{ .c = "mono_time_test.cc" }, &mono_time);
+    _ = r.addGTest(.{ .c = "network_test.cc" }, &network);
+    _ = r.addGTest(.{ .c = "ping_array_test.cc" }, &ping_array);
+    _ = r.addGTest(.{ .c = "DHT_test.cc" }, &DHT);
+    // TODO DHT_fuzz_test
+    // TODO forwarding_fuzz_test
+    _ = r.addGTest(.{ .c = "TCP_connection_test.cc" }, &TCP_connection);
+    _ = r.addGTest(.{ .c = "group_announce_test.cc" }, &group_announce);
+    // TODO group_announce_fuzz_test
+    // duplicated symbols !!
+    // _ = r.addGTest(.{ .c = "friend_connection_test.cc" }, &friend_connection);
 }
