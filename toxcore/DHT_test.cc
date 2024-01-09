988c8818e7413ed8c01c437a090c45285dc1c471
iphydf <iphydf@users.noreply.github.com>
2024-01-09 17:57:27 +0000

    test: Add more unit tests for `add_to_list`.

--- toxcore/DHT_test.cc
+++ toxcore/DHT_test.cc
@@ -1,23 +1,14 @@
 #include "DHT.h"
 
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 #include <algorithm>
 #include <array>
+#include <cstring>
+#include <iomanip>
 
 #include "crypto_core.h"
-
-namespace {
-
-using PublicKey = std::array<uint8_t, CRYPTO_PUBLIC_KEY_SIZE>;
-using SecretKey = std::array<uint8_t, CRYPTO_SECRET_KEY_SIZE>;
-
-struct KeyPair {
-    PublicKey pk;
-    SecretKey sk;
-
-    explicit KeyPair(const Random *rng) { crypto_new_keypair(rng, pk.data(), sk.data()); }
-};
 
 template <typename T, size_t N>
 std::array<T, N> to_array(T const (&arr)[N])
@@ -27,11 +18,195 @@ std::array<T, N> to_array(T const (&arr)[N])
     return stdarr;
 }
 
+template <size_t N, typename T, typename... Args>
+auto array_of(T &&make, Args... args)
+{
+    std::array<typename std::result_of<T(Args...)>::type, N> arr;
+    for (auto &elem : arr) {
+        elem = make(args...);
+    }
+    return arr;
+}
+
+template <typename T, typename... Args>
+auto vector_of(size_t n, T &&make, Args... args)
+{
+    std::vector<typename std::result_of<T(Args...)>::type> vec(n);
+    for (auto &elem : vec) {
+        elem = make(args...);
+    }
+    return vec;
+}
+
+template <typename Container, typename Less>
+Container sorted(Container arr, Less less)
+{
+    std::sort(arr.begin(), arr.end(), less);
+    return arr;
+}
+
+struct PublicKey : private std::array<uint8_t, CRYPTO_PUBLIC_KEY_SIZE> {
+    using Base = std::array<uint8_t, CRYPTO_PUBLIC_KEY_SIZE>;
+
+    using Base::begin;
+    using Base::data;
+    using Base::end;
+    using Base::size;
+    using Base::operator[];
+
+    PublicKey() = default;
+    PublicKey(uint8_t const (&arr)[CRYPTO_PUBLIC_KEY_SIZE])
+        : PublicKey(to_array(arr))
+    {
+    }
+    PublicKey(std::array<uint8_t, CRYPTO_PUBLIC_KEY_SIZE> const &arr)
+    {
+        std::copy(arr.begin(), arr.end(), begin());
+    }
+
+    PublicKey(std::initializer_list<uint8_t> const &arr)
+    {
+        std::copy(arr.begin(), arr.end(), begin());
+    }
+
+    Base const &base() const { return *this; }
+};
+
+static bool operator!=(PublicKey const &pk1, PublicKey const &pk2)
+{
+    return pk1.base() != pk2.base();
+}
+
+static bool operator==(PublicKey const &pk1, PublicKey const &pk2)
+{
+    return pk1.base() == pk2.base();
+}
+
+static bool operator==(PublicKey::Base const &pk1, PublicKey const &pk2)
+{
+    return pk1 == pk2.base();
+}
+
+static std::ostream &operator<<(std::ostream &out, PublicKey const &pk)
+{
+    out << '"';
+    for (uint8_t byte : pk) {
+        out << std::setw(2) << std::setfill('0') << std::hex << uint32_t(byte);
+    }
+    out << '"';
+    return out;
+}
+
+static bool operator==(Node_format const &a, Node_format const &b)
+{
+    return std::memcmp(&a, &b, sizeof(Node_format)) == 0;
+}
+
+static std::ostream &operator<<(std::ostream &out, IP const &v)
+{
+    Ip_Ntoa ip_str;
+    out << '"' << net_ip_ntoa(&v, &ip_str) << '"';
+    return out;
+}
+
+static std::ostream &operator<<(std::ostream &out, IP_Port const &v)
+{
+    return out << "IP_Port{\n"
+               << "        ip = " << v.ip << ",\n"
+               << "        port = " << std::dec << std::setw(0) << v.port << " }";
+}
+
+static std::ostream &operator<<(std::ostream &out, Node_format const &v)
+{
+    return out << "\n    Node_format{\n"
+               << "      public_key = " << PublicKey(v.public_key) << ",\n"
+               << "      ip_port = " << v.ip_port << " }";
+}
+
+PublicKey random_pk(const Random *rng);
 PublicKey random_pk(const Random *rng)
 {
     PublicKey pk;
     random_bytes(rng, pk.data(), pk.size());
     return pk;
+}
+
+IP_Port random_ip_port(const Random *rng);
+IP_Port random_ip_port(const Random *rng)
+{
+    IP_Port ip_port;
+    ip_port.ip.family = net_family_ipv4();
+    ip_port.ip.ip.v4.uint8[0] = 192;
+    ip_port.ip.ip.v4.uint8[1] = 168;
+    ip_port.ip.ip.v4.uint8[2] = 0;
+    ip_port.ip.ip.v4.uint8[3] = random_u08(rng);
+    ip_port.port = random_u16(rng);
+    return ip_port;
+}
+
+Node_format random_node_format(const Random *rng);
+Node_format random_node_format(const Random *rng)
+{
+    Node_format node;
+    auto const pk = random_pk(rng);
+    std::copy(pk.begin(), pk.end(), node.public_key);
+    node.ip_port = random_ip_port(rng);
+    return node;
+}
+
+namespace {
+
+using ::testing::Each;
+using ::testing::ElementsAre;
+using ::testing::Eq;
+using ::testing::PrintToString;
+
+using SecretKey = std::array<uint8_t, CRYPTO_SECRET_KEY_SIZE>;
+
+struct KeyPair {
+    PublicKey pk;
+    SecretKey sk;
+
+    explicit KeyPair(const Random *rng) { crypto_new_keypair(rng, pk.data(), sk.data()); }
+};
+
+class increasing_ip_port {
+    uint8_t start_;
+    const Random *rng_;
+
+public:
+    explicit increasing_ip_port(uint8_t start, const Random *rng)
+        : start_(start)
+        , rng_(rng)
+    {
+    }
+
+    IP_Port operator()()
+    {
+        IP_Port ip_port;
+        ip_port.ip.family = net_family_ipv4();
+        ip_port.ip.ip.v4.uint8[0] = 192;
+        ip_port.ip.ip.v4.uint8[1] = 168;
+        ip_port.ip.ip.v4.uint8[2] = 0;
+        ip_port.ip.ip.v4.uint8[3] = start_++;
+        ip_port.port = random_u16(rng_);
+        return ip_port;
+    }
+};
+
+TEST(IdClosest, KeyIsClosestToItself)
+{
+    const Random *rng = system_random();
+    ASSERT_NE(rng, nullptr);
+
+    PublicKey pk0 = random_pk(rng);
+    PublicKey pk1;
+    do {
+        // Get a random key that's not the same as pk0.
+        pk1 = random_pk(rng);
+    } while (pk0 == pk1);
+
+    EXPECT_EQ(id_closest(pk0.data(), pk0.data(), pk1.data()), 1);
 }
 
 TEST(IdClosest, IdenticalKeysAreSameDistance)
@@ -41,9 +216,8 @@ TEST(IdClosest, IdenticalKeysAreSameDistance)
 
     PublicKey pk0 = random_pk(rng);
     PublicKey pk1 = random_pk(rng);
-    PublicKey pk2 = pk1;
 
-    EXPECT_EQ(id_closest(pk0.data(), pk1.data(), pk2.data()), 0);
+    EXPECT_EQ(id_closest(pk0.data(), pk1.data(), pk1.data()), 0);
 }
 
 TEST(IdClosest, DistanceIsCommutative)
@@ -73,18 +247,18 @@ TEST(IdClosest, DistanceIsCommutative)
 
 TEST(IdClosest, SmallXorDistanceIsCloser)
 {
-    PublicKey const pk0 = {{0xaa}};
-    PublicKey const pk1 = {{0xa0}};
-    PublicKey const pk2 = {{0x0a}};
+    PublicKey const pk0 = {0xaa};
+    PublicKey const pk1 = {0xa0};
+    PublicKey const pk2 = {0x0a};
 
     EXPECT_EQ(id_closest(pk0.data(), pk1.data(), pk2.data()), 1);
 }
 
 TEST(IdClosest, DistinctKeysCannotHaveTheSameDistance)
 {
-    PublicKey const pk0 = {{0x06}};
-    PublicKey const pk1 = {{0x00}};
-    PublicKey pk2 = {{0x00}};
+    PublicKey const pk0 = {0x06};
+    PublicKey const pk1 = {0x00};
+    PublicKey pk2 = {0x00};
 
     for (uint8_t i = 1; i < 0xff; ++i) {
         pk2[0] = i;
@@ -94,14 +268,14 @@ TEST(IdClosest, DistinctKeysCannotHaveTheSameDistance)
 
 TEST(AddToList, OverridesKeysWithCloserKeys)
 {
-    PublicKey const self_pk = {{0xaa}};
+    PublicKey const self_pk = {0xaa};
     PublicKey const keys[] = {
-        {{0xa0}},  // closest
-        {{0x0a}},  //
-        {{0x0b}},  //
-        {{0x0c}},  //
-        {{0x0d}},  //
-        {{0xa1}},  // closer than the 4 keys above
+        {0xa0},  // closest
+        {0x0a},  //
+        {0x0b},  //
+        {0x0c},  //
+        {0x0d},  //
+        {0xa1},  // closer than the 4 keys above
     };
 
     std::array<Node_format, 4> nodes{};
@@ -126,6 +300,108 @@ TEST(AddToList, OverridesKeysWithCloserKeys)
     EXPECT_EQ(to_array(nodes[1].public_key), keys[5]);
     EXPECT_EQ(to_array(nodes[2].public_key), keys[1]);
     EXPECT_EQ(to_array(nodes[3].public_key), keys[2]);
+}
+
+Node_format &fill(Node_format &&v, PublicKey const &pk, IP_Port const &ip_port)
+{
+    std::copy(pk.begin(), pk.end(), v.public_key);
+    v.ip_port = ip_port;
+    return v;
+}
+
+TEST(AddToList, AddsFirstKeysInOrder)
+{
+    const Random *rng = system_random();
+    ASSERT_NE(rng, nullptr);
+
+    // Make cmp_key the furthest away from 00000... as possible, so all initial inserts succeed.
+    PublicKey const cmp_pk{0xff,0xff,0xff,0xff};
+
+    // Generate a bunch of other keys, sorted by distance from cmp_pk.
+    auto const keys
+        = sorted(array_of<20>(random_pk, rng), [&cmp_pk](auto const &pk1, auto const &pk2) {
+              return id_closest(cmp_pk.data(), pk1.data(), pk2.data()) == 1;
+          });
+    auto const ips = array_of<20>(increasing_ip_port(0, rng));
+
+    std::vector<Node_format> nodes(4);
+
+    // Add a bunch of nodes.
+    ASSERT_TRUE(add_to_list(nodes.data(), nodes.size(), keys[2].data(), &ips[2], cmp_pk.data()))
+        << "failed to insert\n  cmp_pk = " << cmp_pk << "\n  pk     = " << keys[2]
+        << "\n  nodes_list = " << PrintToString(nodes);
+    ASSERT_TRUE(add_to_list(nodes.data(), nodes.size(), keys[5].data(), &ips[5], cmp_pk.data()))
+        << "failed to insert\n  cmp_pk = " << cmp_pk << "\n  pk     = " << keys[5]
+        << "\n  nodes_list = " << PrintToString(nodes);
+    ASSERT_TRUE(add_to_list(nodes.data(), nodes.size(), keys[7].data(), &ips[7], cmp_pk.data()))
+        << "failed to insert\n  cmp_pk = " << cmp_pk << "\n  pk     = " << keys[7]
+        << "\n  nodes_list = " << PrintToString(nodes);
+    ASSERT_TRUE(add_to_list(nodes.data(), nodes.size(), keys[9].data(), &ips[9], cmp_pk.data()))
+        << "failed to insert\n  cmp_pk = " << cmp_pk << "\n  pk     = " << keys[9]
+        << "\n  nodes_list = " << PrintToString(nodes);
+
+    // They should all appear in order.
+    EXPECT_THAT(nodes,
+        ElementsAre(fill(Node_format{}, keys[2], ips[2]), fill(Node_format{}, keys[5], ips[5]),
+            fill(Node_format{}, keys[7], ips[7]), fill(Node_format{}, keys[9], ips[9])));
+
+    // Adding another node that's further away will not happen.
+    ASSERT_FALSE(add_to_list(nodes.data(), nodes.size(), keys[10].data(), &ips[10], cmp_pk.data()))
+        << "incorrectly inserted\n  cmp_pk = " << cmp_pk << "\n  pk     = " << keys[10]
+        << "\n  nodes_list = " << PrintToString(nodes);
+
+    // Adding one that's closer will happen.
+    ASSERT_TRUE(add_to_list(nodes.data(), nodes.size(), keys[8].data(), &ips[8], cmp_pk.data()))
+        << "failed to insert\n  cmp_pk = " << cmp_pk << "\n  pk     = " << keys[8]
+        << "\n  nodes_list = " << PrintToString(nodes);
+
+    EXPECT_THAT(nodes,
+        ElementsAre(fill(Node_format{}, keys[2], ips[2]), fill(Node_format{}, keys[5], ips[5]),
+            fill(Node_format{}, keys[7], ips[7]), fill(Node_format{}, keys[8], ips[8])));
+
+    // Adding one that's closer than almost all of them will happen.
+    ASSERT_TRUE(add_to_list(nodes.data(), nodes.size(), keys[4].data(), &ips[4], cmp_pk.data()))
+        << "failed to insert\n  cmp_pk = " << cmp_pk << "\n  pk     = " << keys[4]
+        << "\n  nodes_list = " << PrintToString(nodes);
+
+    EXPECT_THAT(nodes,
+        ElementsAre(fill(Node_format{}, keys[2], ips[2]), fill(Node_format{}, keys[4], ips[4]),
+            fill(Node_format{}, keys[5], ips[5]), fill(Node_format{}, keys[7], ips[7])));
+
+    // Adding one that's closer than all of them will happen.
+    ASSERT_TRUE(add_to_list(nodes.data(), nodes.size(), keys[1].data(), &ips[1], cmp_pk.data()))
+        << "failed to insert\n  cmp_pk = " << cmp_pk << "\n  pk     = " << keys[1]
+        << "\n  nodes_list = " << PrintToString(nodes);
+
+    EXPECT_THAT(nodes,
+        ElementsAre(fill(Node_format{}, keys[1], ips[1]), fill(Node_format{}, keys[2], ips[2]),
+            fill(Node_format{}, keys[4], ips[4]), fill(Node_format{}, keys[5], ips[5])));
+}
+
+TEST(AddToList, KeepsKeysInOrder)
+{
+    const Random *rng = system_random();
+    ASSERT_NE(rng, nullptr);
+
+    // Any random cmp_pk should work.
+    PublicKey const cmp_pk = random_pk(rng);
+
+    auto const by_distance = [&cmp_pk](auto const &node1, auto const &node2) {
+        return id_closest(cmp_pk.data(), node1.public_key, node2.public_key) == 1;
+    };
+
+    // Generate a bunch of other keys, not sorted.
+    auto const nodes = vector_of(16, random_node_format, rng);
+
+    std::vector<Node_format> node_list(4);
+
+    // Add all of them.
+    for (Node_format const &node : nodes) {
+        add_to_list(
+            node_list.data(), node_list.size(), node.public_key, &node.ip_port, cmp_pk.data());
+        // Nodes should always be sorted.
+        EXPECT_THAT(node_list, Eq(sorted(node_list, by_distance)));
+    }
 }
 
 TEST(Request, CreateAndParse)
