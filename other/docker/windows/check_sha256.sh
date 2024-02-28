c49a52682dfee5184f4541a182478ff2770958d6
Restyled.io <commits@restyled.io>
2024-02-28 09:31:18 +0000

    Restyled by shfmt

--- other/docker/windows/check_sha256.sh
+++ other/docker/windows/check_sha256.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 
-check_sha256()
-{
+check_sha256() {
   [ "$ENABLE_HASH_VERIFICATION" = "true" ] && _check_sha256 "$@"
 }
 
-_check_sha256()
-{
-  if ! ( echo "$1  $2" | sha256sum -c --status - )
-  then
+_check_sha256() {
+  if ! (echo "$1  $2" | sha256sum -c --status -); then
     echo "Error: sha256 of $2 doesn't match the known one."
     echo "Expected: $1  $2"
     echo "Got: $(sha256sum "$2")"
