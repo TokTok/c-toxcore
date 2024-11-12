c6ffc46173ba3ea78e216ba8bd35cd029c56ac9b
iphydf <iphydf@users.noreply.github.com>
2024-11-12 20:39:16 +0000

    refactor: Move tox_log_level out into its own file.
    Perhaps we can reuse it in the logger.

---
+++ toxcore/tox_log_level.c
@@ -0,0 +1,23 @@
+#include "tox_log_level.h"
+
+const char *tox_log_level_to_string(Tox_Log_Level value)
+{
+    switch (value) {
+    case TOX_LOG_LEVEL_TRACE:
+        return "TOX_LOG_LEVEL_TRACE";
+
+    case TOX_LOG_LEVEL_DEBUG:
+        return "TOX_LOG_LEVEL_DEBUG";
+
+    case TOX_LOG_LEVEL_INFO:
+        return "TOX_LOG_LEVEL_INFO";
+
+    case TOX_LOG_LEVEL_WARNING:
+        return "TOX_LOG_LEVEL_WARNING";
+
+    case TOX_LOG_LEVEL_ERROR:
+        return "TOX_LOG_LEVEL_ERROR";
+    }
+
+    return "<invalid Tox_Log_Level>";
+}
