04c917eddf360802cf67b9f01e67caa7761991f5
iphydf <iphydf@users.noreply.github.com>
2025-01-27 02:14:53 +0000

    chore: Add iOS deploy workflow.

--- other/deploy/ios.sh
+++ other/deploy/ios.sh
@@ -4,15 +4,17 @@ set -eux -o pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-"$SCRIPT_DIR/deps.sh" macos
+"$SCRIPT_DIR/deps.sh" ios
 
 export PKG_CONFIG_PATH="$PWD/prefix/lib/pkgconfig"
 
-# Build for macOS
+IOS_FLAGS="-miphoneos-version-min=10.0 -arch arm64"
+
+# Build for iOS 10
 cmake \
   -B _build \
   -G Ninja \
-  -DCMAKE_INSTALL_PREFIX="$PWD/toxcore-macos-$(uname -m)" \
+  -DCMAKE_INSTALL_PREFIX="$PWD/toxcore-ios-$(uname -m)" \
   -DCMAKE_BUILD_TYPE=Release \
   -DENABLE_STATIC=OFF \
   -DENABLE_SHARED=ON \
@@ -21,7 +23,11 @@ cmake \
   -DBOOTSTRAP_DAEMON=OFF \
   -DUNITTEST=OFF \
   -DMIN_LOGGER_LEVEL=TRACE \
-  -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15
+  -DCMAKE_C_FLAGS="$IOS_FLAGS" \
+  -DCMAKE_CXX_FLAGS="$IOS_FLAGS" \
+  -DCMAKE_EXE_LINKER_FLAGS="$IOS_FLAGS" \
+  -DCMAKE_SHARED_LINKER_FLAGS="$IOS_FLAGS" \
+  -DCMAKE_OSX_SYSROOT="$(xcrun --sdk iphoneos --show-sdk-path)"
 
 cmake --build _build
 cmake --install _build
